a53c17e04d3ff90fd0f4ecb13dfb029a8ae71533
Carwest Sung <carwestsam@gmail.com>
2015-07-29 11:29:44 +0800

    Welcome Message

---
+++ src/com/twu/biblioteca/FrontEnd.java
@@ -0,0 +1,11 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by carwest on 15-7-29.
+ */
+public class FrontEnd {
+
+    public void start() {
+        System.out.println("Welcome to Biblioteca");
+    }
+}
