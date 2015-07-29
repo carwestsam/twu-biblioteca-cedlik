93036a38ad4652c5ea2f5a67ea650641642c4e3b
Carwest Sung <carwestsam@gmail.com>
2015-07-29 13:23:44 +0800

    update list books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,16 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.controller.BookManager;
+import com.twu.biblioteca.model.Book;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BookManager bookManager = new BookManager();
+        bookManager.add(new Book("C++"));
+        bookManager.add(new Book("python"));
+        FrontEnd frontEnd = new FrontEnd(bookManager);
+        frontEnd.start();
+        frontEnd.listBooks();
     }
 }
